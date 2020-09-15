980529445ed19151e170a106f56faaa5fcb26d83
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2020-09-16 03:59:00 +0530

    home page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'rayblinds';
+  ngAfterViewInit() {
+    var dropToggle = $('.menu_right > li').has('ul').children('a');
+    dropToggle.on('click', function() {
+      dropToggle.not(this).closest('li').find('ul').slideUp(200);
+      $(this).closest('li').children('ul').slideToggle(200);
+      return false;
+    });
+
+    $( ".toggle_icon" ).on('click', function() {
+      $( 'body' ).toggleClass( "open" );
+    });
+
+
+  }
 }
